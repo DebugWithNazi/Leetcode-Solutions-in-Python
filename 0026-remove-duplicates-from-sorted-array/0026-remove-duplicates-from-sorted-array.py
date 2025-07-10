5a864fcfff642496e71e4566e6f0581c860cff81
Nazia Ramzan Khan <121787737+DebugWithNazi@users.noreply.github.com>
2025-07-10 11:02:50 +0500

    Time: 7 ms (26.77%), Space: 13 MB (99.86%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        left = 0 
+        right = left + 1
+        count = 1
+
+        while right < len(nums): #1
+            if nums[left] != nums[right]:
+               left += 1
+               nums[left] = nums[right]
+               count += 1
+            right += 1 #2
+        return count
+
+                
+        
