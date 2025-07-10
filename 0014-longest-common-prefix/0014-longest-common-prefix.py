e5ca969e5599301f032e87901696f6115b69ba76
Nazia Ramzan Khan <121787737+DebugWithNazi@users.noreply.github.com>
2025-07-10 14:26:16 +0500

    Time: 1 ms (46.58%), Space: 12.6 MB (26.32%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -15,43 +15,7 @@ class Solution(object):
               j += 1
 
         return final if final else ""
-                 
 
-                
-                 
-              
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        # strs.sort()
-
-        # first = strs[0]
-        # last = strs[len(strs)-1]
-        # i, j = 0,0
-        # final = ""
-        # while i < len(first) and j < len(last):
-        #       if first[i] != last[j]:
-        #          return final
-        #       final += first[i]
-        #       i += 1
-        #       j += 1
-        # return final
                  
 
 
