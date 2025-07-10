c240fc990eee7d97d40cb7e3f91b99720da05933
Nazia Ramzan Khan <121787737+DebugWithNazi@users.noreply.github.com>
2025-07-10 13:41:03 +0500

    Time: 183 ms (6.81%), Space: 16.4 MB (5.07%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -18,21 +18,7 @@ class Solution(object):
 
 
 
-        # t = ""
-
-        # for i in range(len(s)):
-        #     if s[i].isalnum():
-        #         t += s[i].lower()
-        
-        # left = 0
-        # right = len(t)-1
-
-        # while left <= right:
-        #     if t[left] != t[right]:
-        #         return False
-        #     left += 1
-        #     right -= 1
-        # return True
+      
         
 
 
