f07891e92b3440e0f3484fbc84a587ac399c80c4
Nazia Ramzan Khan <121787737+DebugWithNazi@users.noreply.github.com>
2025-07-18 13:38:30 +0500

    Time: 29 ms (66.1%), Space: 37.2 MB (9.64%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -4,7 +4,7 @@ class Solution:
         index_map = {}
 
         for i in range(len(nums)):
-            if nums[i] in index_map and abs(i - index_map[nums[i]]) <= k:
+            if nums[i] in index_map and i - index_map[nums[i]] <= k:
                 return True
             index_map[nums[i]] = i
 
