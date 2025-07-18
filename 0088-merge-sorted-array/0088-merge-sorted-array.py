47975ddea970a8c0d29c8a1efb8aa1a1a1420174
Nazia Ramzan Khan <121787737+DebugWithNazi@users.noreply.github.com>
2025-07-18 13:49:09 +0500

    Time: 0 ms (100%), Space: 17.6 MB (99.33%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -1,46 +1,31 @@
-class Solution(object):
-    def merge(self, nums1, m, nums2, n):
-        last = m + n - 1
-     
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        # Start filling nums1 from the end
+            i = m - 1        # pointer for nums1's real elements
+            j = n - 1        # pointer for nums2
+            k = m + n - 1    # pointer for final position in nums1
 
-        while m>0 and n >0:
-            if nums1[m-1] > nums2[n-1]:
-                nums1[last] = nums1[m-1]
-                last -= 1
-                m -= 1
-            else:
-                nums1[last] = nums2[n-1]
-                last -= 1
-                n -= 1
-        
-        while n > 0:
-            nums1[last] = nums2[n-1]
-            n -= 1
-            last -= 1
-            
+            # Merge from back to front
+            while i >= 0 and j >= 0:
+                if nums1[i] > nums2[j]:
+                    nums1[k] = nums1[i]
+                    i -= 1
+                else:
+                    nums1[k] = nums2[j]
+                    j -= 1
+                k -= 1
 
-        # left = 0
-        # right = 0
-        # if not nums1:
-        #     return nums2
-        # if not nums2:
-        #     return nums1
-       
+            # If any elements left in nums2, copy them
+            while j >= 0:
+                nums1[k] = nums2[j]
+                j -= 1
+                k -= 1
 
-        # while left < m:
-        #     if nums1[left] >= nums2[right]:
-        #         nums1[left],nums2[right]= nums2[right],nums1[left]
-        #         nums2.sort()
-        #     left += 1
-        
-        # while right < n:
-        #     nums1[left] = nums2[right]
-        #     left += 1
-        #     right += 1
-        
-        # return nums1
 
-                
-            
-            
+
+
+
+
+
+
         
