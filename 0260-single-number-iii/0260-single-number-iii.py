dd67c560a81533d4ef1b0c3c530a9e600f1fcee1
Nazia Ramzan Khan <121787737+DebugWithNazi@users.noreply.github.com>
2025-07-21 13:56:35 +0500

    Time: 2 ms (74.88%), Space: 19.1 MB (86%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.py
+++ 0260-single-number-iii/0260-single-number-iii.py
@@ -1,18 +1,22 @@
-class Solution(object):
-    def singleNumber(self, nums):
-        
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
         xor = nums[0]
+
         for i in range(1,len(nums)):
             xor = xor ^ nums[i]
-        
-        diff_bit = xor & (-xor)
-        group1 = 0
-        group2 = 0 
 
-        for num in nums: 
-            if num & diff_bit:
+        diff = xor & -xor
+
+        group1 = 0
+        group2 = 0
+
+        for num in nums:
+            if num & diff:
                 group1 = group1 ^ num
             else:
                 group2 = group2 ^ num
-
+        
         return [group1,group2]
+
+        
+                
