b084f9535a364bc785331c669e432051a4a0e58b
Nazia Ramzan Khan <121787737+DebugWithNazi@users.noreply.github.com>
2025-07-22 15:34:46 +0500

    Time: 31 ms (85.52%), Space: 18.4 MB (35.57%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -1,22 +1,24 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        if len(p) > len(s): return []
-        pCount, sCount = {},{}
-
-        for i in range(len(p)):
-            pCount[p[i]] = 1 + pCount.get(p[i],0)
-            sCount[s[i]] = 1 + sCount.get(s[i],0)
+        if len(p) > len(s):
+            return []
+        countP = {}
+        countS = {}
+        res = []
         
-        res = [0] if sCount == pCount else []
+        for i in range(len(p)):
+            countP[p[i]] = countP.get(p[i],0) + 1
+            countS[s[i]] = countS.get(s[i],0) + 1
+        
+        res = [0] if countP == countS else []
         l = 0
-        for r in range(len(p),len(s)):
-            sCount[s[r]] = 1 + sCount.get(s[r],0)
-            sCount[s[l]] -= 1 
-
-            if sCount[s[l]] == 0:
-                sCount.pop(s[l])
+        for i in range(len(p),len(s)):
+            countS[s[i]] = countS.get(s[i],0) + 1
+            countS[s[l]] -= 1
+            
+            if countS[s[l]] == 0:
+                del countS[s[l]]
             l += 1
-
-            if sCount == pCount:
+            if countS == countP:
                 res.append(l)
-        return res        
+        return res
