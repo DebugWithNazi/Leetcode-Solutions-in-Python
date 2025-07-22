b586b2c41a0a43bb874d23fc01035493b08538d1
Nazia Ramzan Khan <121787737+DebugWithNazi@users.noreply.github.com>
2025-07-22 11:39:32 +0500

    Time: 26 ms (84.32%), Space: 18.8 MB (30%) - LeetHub

--- 0659-split-array-into-consecutive-subsequences/0659-split-array-into-consecutive-subsequences.py
+++ 0659-split-array-into-consecutive-subsequences/0659-split-array-into-consecutive-subsequences.py
@@ -1,23 +1,27 @@
-class Solution(object):
-    def isPossible(self, nums):
+class Solution:
+    def isPossible(self, nums: List[int]) -> bool:
         freq = Counter(nums)
-        appendFreq = defaultdict(int)
+        need = defaultdict(int)
 
         for num in nums:
             if freq[num] == 0:
                 continue
             
-            if appendFreq[num - 1] > 0:
-                appendFreq[num - 1] -= 1
-                appendFreq[num] += 1
+            elif need[num] > 0:
                 freq[num] -= 1
+                need[num] -= 1
+                need[num+1] += 1
             
-            elif freq[num+1] > 0 and freq[num+2] >0:
-                 freq[num] -=1 
-                 freq[num+1] -= 1
-                 freq[num+2]-=1
-                 appendFreq[num+2] += 1
+            elif freq[num+1] > 0 and freq[num+2] > 0:
+                freq[num] -= 1
+                freq[num+1] -= 1
+                freq[num+2] -= 1
+                need[num+3] += 1
             
             else:
                 return False
+        
         return True
+            
+
+        
