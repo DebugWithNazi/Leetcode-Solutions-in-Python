296621ddc4e7c4e6c930089ce35721d08f99a549
Nazia Ramzan Khan <121787737+DebugWithNazi@users.noreply.github.com>
2025-07-23 15:34:47 +0500

    Time: 0 ms (100%), Space: 17.7 MB (89.45%) - LeetHub

--- 0926-find-and-replace-pattern/0926-find-and-replace-pattern.py
+++ 0926-find-and-replace-pattern/0926-find-and-replace-pattern.py
@@ -1,30 +1,24 @@
 class Solution:
     def findAndReplacePattern(self, words: List[str], pattern: str) -> List[str]:
-        patDict = {}
-        count = 1
-        string = []
         final = []
-        for char in pattern:
-            if char not in patDict:
-                patDict[char] = count
-                string.append(count)
-                count += 1
-            else:
-                string.append(patDict[char])
-        
-        for word in words:
-            newString = []
+       
+        def patternMatching(value):
             count = 1
+            string = []
             patDict = {}
-            for char in word:
+
+            for char in value:
                 if char not in patDict:
                     patDict[char] = count
-                    newString.append(count)
                     count += 1
-                else:
-                    newString.append(patDict[char])
-            if string == newString:
-                final.append(word)
+                string.append(patDict[char])
+            return string
+        
+        matching = patternMatching(pattern)
+        for word in words:
+            if matching == patternMatching(word):
+               final.append(word)
+       
         return final 
                 
          
