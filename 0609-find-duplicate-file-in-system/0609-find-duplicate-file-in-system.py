b63c666a937face1cca4595d1726821617ec7979
Nazia Ramzan Khan <121787737+DebugWithNazi@users.noreply.github.com>
2025-08-07 13:48:03 +0500

    Time: 19 ms (70.9%), Space: 25.1 MB (72.83%) - LeetHub

--- 0609-find-duplicate-file-in-system/0609-find-duplicate-file-in-system.py
+++ 0609-find-duplicate-file-in-system/0609-find-duplicate-file-in-system.py
@@ -13,3 +13,8 @@ class Solution:
                 dictionary[content].append(full_path)
         
         return [group for group in dictionary.values() if len(group) > 1]
+
+
+# time O(L * m * n) L is len of each string, m is each file, n in content in file
+
+# space O(m*n) in dict file * content
