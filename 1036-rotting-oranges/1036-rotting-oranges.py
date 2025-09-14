f5c6b00a0be2a2820e57bb0096797603f43c5eec
Nazia Ramzan Khan <121787737+DebugWithNazi@users.noreply.github.com>
2025-09-14 09:34:12 +0500

    Time: 7 ms (51.41%), Space: 12.5 MB (16.14%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.py
+++ 1036-rotting-oranges/1036-rotting-oranges.py
@@ -3,7 +3,7 @@ class Solution(object):
         queue = deque()
         n = len(grid)
         m = len(grid[0])
-        time, fresh = 0,0
+        time, fresh = 0, 0
 
         for i in range(n):
             for j in range(m):
@@ -11,31 +11,18 @@ class Solution(object):
                     fresh += 1
                 elif grid[i][j] == 2:
                     queue.append((i,j))
-        
-        while queue and fresh > 0: 
+
+        while queue and fresh > 0:
             for _ in range(len(queue)):
-                r,c = queue.popleft()
-                
-                for dr,dc in [(-1,0),(1,0),(0,1),(0,-1)]:
-                    nr, nc = r+dr, c+dc
-                    if 0 <= nr <n and 0 <= nc < m and grid[nr][nc] == 1:
-                        grid[nr][nc] = 2
-                        fresh -= 1
-                        queue.append((nr,nc))
-            time += 1            
-        
+               r,c = queue.popleft()
+
+               for dr,dc in [(-1,0),(1,0),(0,-1),(0,1)]:
+                   nr, nc = r+dr , c+dc
+                   if  0 <= nr < n and 0 <= nc < m and grid[nr][nc] == 1:
+                       grid[nr][nc] = 2
+                       fresh -= 1
+                       queue.append((nr,nc))
+            time += 1
+
         return time if fresh == 0 else -1
-
-
-
-
-
-
-
-
-
-
-
-
-        
-        
+            
